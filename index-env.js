ece76ff864f9ab8cd24aac3f67b0752e0ca6c7fe
Scott Lively <scott.lively@gmail.com>
2015-01-07 13:16:30 -0600

    allow fallback to config.json

--- index-env.js
+++ index-env.js
@@ -6,29 +6,40 @@ module.exports = function indexEnvDef(opts, cb) {
         env = process.env.NODE_ENV || 'development',
         inputFilePath = (o.inputFilePath || 'index.ejs'),
         configFolderPath = (o.configFolderPath || 'config') + '/',
-        configFilePath = configFolderPath + ((env && env !== 'development') ? 'config.' + env + '.json' : 'config.json');
+        defaultConfigFilePath = configFolderPath + 'config.json';
+        configFilePath = ((env && env !== 'development') ? configFolderPath + ('config.' + env + '.json') : defaultConfigFilePath);
 
-    fs.readFile(configFilePath, function(err, result) {
-        var config;
-
-        if (err) {
-            err.message = 'Reading config file: ' + configFilePath + '\n' + err.message;
-            return cb(err);
-        }
-
-        try {
-            config = JSON.parse(result);
-        } catch(err) {
-            err.message = 'Parsing config file: ' + configFilePath + '\n' + err.message;
-            return cb(err);
-        }
-
-        ejs.renderFile(inputFilePath, { config: config }, function(err,result) {
-            if (err) {
-                err.message = 'Rendering input file: ' + inputFilePath + '\n' + err.message;
+    fs.exists(configFilePath, function(exists) {
+        if (!exists) {
+            if (configFilePath === defaultConfigFilePath) {
+                var err = new Error('File ' + configFilePath + ' not found.');
                 return cb(err);
             }
-            cb(null, result);
+            configFilePath = defaultConfigFilePath;
+        }
+
+        fs.readFile(configFilePath, function(err, result) {
+            var config;
+
+            if (err) {
+                err.message = 'Reading config file: ' + configFilePath + '\n' + err.message;
+                return cb(err);
+            }
+
+            try {
+                config = JSON.parse(result);
+            } catch(err) {
+                err.message = 'Parsing config file: ' + configFilePath + '\n' + err.message;
+                return cb(err);
+            }
+
+            ejs.renderFile(inputFilePath, { config: config }, function(err,result) {
+                if (err) {
+                    err.message = 'Rendering input file: ' + inputFilePath + '\n' + err.message;
+                    return cb(err);
+                }
+                cb(null, result);
+            });
         });
     });
 };
